a1f7524041d191676237e59ac00e5e2d8b4aa342
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2025-01-23 15:29:17 +0700

    Provider Set

---
+++ simple/foobar.go
@@ -0,0 +1,10 @@
+package simple
+
+type FooBarService struct {
+	*FooService
+	*BarService
+}
+
+func NewFooBarService(foo *FooService, bar *BarService) *FooBarService {
+	return &FooBarService{FooService: foo, BarService: bar}
+}
--- simple/injector.go
+++ simple/injector.go
@@ -15,4 +15,12 @@ func InitializedDatabase() *DatabaseRepository {
 	return nil
 }
 
+var fooSet = wire.NewSet(NewFooRepository, NewFooService)
+var barSet = wire.NewSet(NewBarRepository, NewBarService)
+
+func InitializedFooBar() *FooBarService {
+	wire.Build(fooSet, barSet, NewFooBarService)
+	return nil
+}
+
 // habis itu eksekusi pake cmd wire di folder package yg ada injectornya, file generatenya punya nama wire_gen.go
