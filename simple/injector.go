2923953ea1bf1ea07fa9c0dd0275a43c4faab273
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2025-01-24 15:26:31 +0700

    Struct Provider

--- simple/injector.go
+++ simple/injector.go
@@ -39,4 +39,9 @@ func InitializedHelloService() *HelloService {
 	return nil
 }
 
+func InitializedFooBarStruct() *FooBar {
+	wire.Build(NewFoo, NewBar, wire.Struct(new(FooBar), "*"))
+	return nil
+}
+
 // habis itu eksekusi pake cmd wire di folder package yg ada injectornya, file generatenya punya nama wire_gen.go
