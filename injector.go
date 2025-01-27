e75e3881f9cc178e6fa3a6edf1cccbe1d762f18d
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2025-01-27 23:53:40 +0700

    Dependency Injection RESTful API

---
+++ injector.go
@@ -0,0 +1,39 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"net/http"
+	"rest-api-pzn-go/app"
+	"rest-api-pzn-go/controller"
+	"rest-api-pzn-go/middleware"
+	"rest-api-pzn-go/repository"
+	"rest-api-pzn-go/service"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/google/wire"
+	"github.com/julienschmidt/httprouter"
+)
+
+var categorySet = wire.NewSet(
+	repository.NewCategoryRepository,
+	wire.Bind(new(repository.CategoryRepository), new(*repository.CategoryRepositoryImpl)),
+	service.NewCategoryService,
+	wire.Bind(new(service.CategoryService), new(*service.CategoryServiceImpl)),
+	controller.NewCategoryController,
+	wire.Bind(new(controller.CategoryController), new(*controller.CategoryControllerImpl)),
+)
+
+func InitializedServer() *http.Server {
+	wire.Build(
+		app.NewDB,
+		validator.New,
+		categorySet,
+		app.NewRouter,
+		wire.Bind(new(http.Handler), new(*httprouter.Router)),
+		middleware.NewAuthMiddleware,
+		NewServer,
+	)
+	return nil
+}
--- main.go
+++ main.go
@@ -2,32 +2,21 @@ package main
 
 import (
 	"net/http"
-	"rest-api-pzn-go/app"
-	"rest-api-pzn-go/controller"
 	"rest-api-pzn-go/helper"
 	"rest-api-pzn-go/middleware"
-	"rest-api-pzn-go/repository"
-	"rest-api-pzn-go/service"
 
-	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-
-	db := app.NewDB()
-	validate := validator.New()
-	categoryRepository := repository.NewCategoryRepository()
-	categoryService := service.NewCategoryService(categoryRepository, db, validate)
-	categoryController := controller.NewCategoryController(categoryService)
-	router := app.NewRouter(categoryController)
-
-	server := http.Server{
+func NewServer(authMiddleware *middleware.AuthMiddleware) *http.Server {
+	return &http.Server{
 		Addr:    "localhost:3000",
-		Handler: middleware.NewAuthMiddleware(router),
+		Handler: authMiddleware,
 	}
+}
 
+func main() {
+	server := InitializedServer()
 	err := server.ListenAndServe()
 	helper.PanicIfError(err)
-
 }
